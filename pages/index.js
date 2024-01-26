b15f2b0e7228b5ae93b52291c503908d39d88bf0
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-01-26 10:18:59 +0000

    pages and styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+//Internal import
+import { Footer, HeroSection, Header, Card } from "../components/index";
+
+const home = () => {
+  return (
+    <div className="bg-[#1A1A1A]">
+      <Header />
+      <HeroSection />
+      <Card />
+      <Footer />
+    </div>
+  )
 };
 
-export default index;
+export default home;
