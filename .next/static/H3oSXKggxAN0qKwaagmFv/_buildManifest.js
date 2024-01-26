316ce91446a643b8c3cd7e567c2382dc106dbdec
Ananya G Gouda <114345744+ananya-gouda@users.noreply.github.com>
2024-01-26 16:11:51 +0000

    TokenBalance TransactionStatus

---
+++ .next/static/H3oSXKggxAN0qKwaagmFv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-19386e43b26e00e7.js"],"/_error":["static/chunks/pages/_error-25bbc618360854a5.js"],"/tokens":["static/chunks/pages/tokens-6fea57ffc50c8b5c.js"],sortedPages:["/","/_app","/_error","/tokens"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- components/TokenBalance.js
+++ components/TokenBalance.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { ethers } from "ethers";
+import toast, {Toaster} from "react-hot-toast";
+import {
 
-const TokenBalance = () => {
-  return <div>TokenBalance</div>;
+  ClipboardIcon,
+  ClipboardCheckIcon,
+  PlusIcon,
+
+} from "@heroicons/react/outline";
+
+//INTERNAL IMPORT
+import { TransactionStatus } from "./index";
+
+import { getTokenAddress, getTokenBalance, increaseAllowance,} from "../utils/context";
+import { addRequestMeta } from "next/dist/server/request-meta";
+import { Divider } from "@nextui-org/react";
+
+const TokenBalance = ({ name, walletAddress }) => {
+
+  const [balnce, setBalance ] = useState("-");
+  const [tokenAddress, setTokenAddress] = useState();
+  const [copyIcon, setCopyIcon] = useState( {icon: ClipboardIcon});
+  const [txPending, setTcPending] = useState(false);
+
+  const notifyError = (msg) => toast.error(msg, { duration: 6000 });
+  const notifySuccess = () => toast.success("Transation completed.");
+
+  useEffect(() => {
+    if(name && walletAddress) {
+      fetchTokenBalance();
+      fetchTokenAddress();
+    } else setBalance("-");
+  }, [name, walletAddress]);
+
+  async function fetchTokenBalance() {
+    const bal = await getTokenBalance(name, walletAddress);
+    console.log(bal);
+    const fBal = ethers.utils.formatUnits(bal.toString(), 18);
+    setBalance(fBal.toString());
+  };
+
+  async function fetchTokenAddress() {
+    const address = await getTokenAddress(name);
+    setTokenAddress(address);
+  }
+
+  return (
+    <div className="flex mx-2 border-[1px] rounded-l rounded-r-lg border-[#7765F3]">
+      <div className="flex items-center bg-zinc-900 text-zinc-300 w-fit p-2 px-3 rounded-l-lg">
+        <p className="text-sm">{name}</p>
+        <p className="bg-zinc-800 p-0.5 px-3 ml-3 rounded-lg text-zinc-100">{balance}</p>
+      </div>
+
+      <div className="flex items-center p-2 px-2 bg-[#7765F3] rounded-r-lg">
+        <copyIcon.icon 
+          className="h-6 cursor-pointer"
+          onClick={() => {
+            navigator.clipboard.writeText(tokenAddress);
+            setCopyIcon({ icon: ClipboardCheckIcon });
+          }}
+        />
+      </div>
+
+      {txPending && <TransactionStatus />}
+
+      <Toaster />
+    </div>
+  );
+
+  // return <div>TokenBalance</div>;
 };
 
 export default TokenBalance;
