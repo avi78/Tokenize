db2266cabb5664761e5fe9206687c7b4c881f99f
Ananyaa059 <99309545+Ananyaa059@users.noreply.github.com>
2024-01-28 07:51:03 +0000

    singlecard_incomplete

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  getTokenAddress,
+  getTokenBalance,
+  increaseAllowance,
+} from "../utils/context";
+import { ethers } from "ethers";
+import TransactionStatus from "./TransactionStatus";
+import toast, { Toaster } from "react-hot-toast";
 
-const SingleCard = () => {
-  return <div>SingleCard</div>;
+import{
+  ClipboardIcon,
+  ClipboardCheckIcon,
+  PlusIcon,
+} from "@heroicons/react/outline";
+
+const SingleCard = ({ index, name, walletAddress }) => {
+  const [balance, setbalance] = useState("-");
+  const [tokenAddress, setTokenAddress] = useState();
+
+  const [copyIcon, setCopyIcon] = usestate({ icon: ClipboardIcon });
+
+  const [txPending, setTxPending] = useState(false);
+
+  const notifyError = (msg) => toast.error(msg, {duration: 6000});
+  const notifySuccess = () => toast.success("Transaction completed.");
+
+  useEffect(() => {
+    if (name && walletAddress) {
+      fetchTokenBalance();
+      fetchTokenAddress();    
+      } else setbalance("-");
+  }, [name, walletAddress]);
+
+  async function fetchTokenBalance() {
+    const bal = await getTokenBalance(name, walletAddress);
+
+    const fbal = ethers.utils.formatUnits(bal.toString(), 18);
+    setBalance(fbal.toString());
+  }
+
+  async function fetchTokenAddress() {
+    const address = await getTokenAddress(name);
+    setTokenAddress(address);
+  }
+
+  return (
+    <article className="  flex flex-col bg-[#212429]">
+      <a
+        rel="noopener noreferrer"
+        href="#"
+        aria-label="Lorem ipsum dolor sit amet"
+      >
+        <img
+          alt=""
+          className="object-cover w-full h-62 bg-gray-500"
+          src={'img/${index + 1}.png'}
+        />
+      </a>
+    </article>
+  )
 };
 
 export default SingleCard;
