17046f1ebe983c25e09f1bb02bb799c268fbd417
Ananyaa059 <99309545+Ananyaa059@users.noreply.github.com>
2024-01-28 08:46:55 +0000

    singlecard_done

--- components/SingleCard.js
+++ components/SingleCard.js
@@ -54,11 +54,50 @@ const SingleCard = ({ index, name, walletAddress }) => {
         <img
           alt=""
           className="object-cover w-full h-62 bg-gray-500"
-          src={'img/${index + 1}.png'}
+          src={`img/${index + 1}.png`}
         />
       </a>
+      <div className="flex flex-col flex-1 p-6">
+
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          aria-label="Lorem ipsum dolor sit amet"
+        ></a>
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          className="text-xs tracki uppercase hover:underline text-[#7765f3]"
+        >
+          {name} 10 M Supply
+        </a>
+
+        <h3 className="flex-1 py-2 text-lg font-semibold leadi">
+          Get {name} token, limited supply avalible
+        </h3>
+      
+
+        <div className="flex mx-2 pt-[10px]">
+          <div className="flex items-center bg-zinc-900 text-zinc-300 w-fit p-2 px3 rounded-l-lg">
+            <p className="text-sm">{name}</p>
+            <p className="bg-zinc-800 p-0.5 px-3 ml-3 rounded-lg text-zinc-100">
+              {balance}
+            </p>
+          </div>
+
+          <div className="flex items-center p-2 px-2  bg-[#7765F3] rounded-r-lg">
+            <copyIcon.icon
+              className="h-6 cursor-pointer"
+              onClick={() => {
+                navigator.clipboard.writeText(tokenAddress);
+                setCopyIcon({ icon: ClipboardCheckIcon });
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </article>
-  )
+  );
 };
 
 export default SingleCard;
